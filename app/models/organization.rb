bee5eb75fdbde975e91029f68e2f1c1a886c64b9
Dunya Kirkali <dunyakirkali@gmail.com>
2015-11-07 02:19:30 +0100

    Org show implementation

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,4 +2,21 @@ class Organization < ActiveRecord::Base
   def sync!
     OrganizationSyncer.perform_async(id)
   end
+
+  def employees_of_the_year(year)
+    [
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      ''
+    ]
+  end
 end
