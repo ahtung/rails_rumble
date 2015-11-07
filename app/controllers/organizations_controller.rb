eaf9122b47e64b97082cd0f520f80875b9fe1a2c
Dunya Kirkali <dunyakirkali@gmail.com>
2015-11-07 01:44:21 +0100

    Add organizations#show

---
+++ app/controllers/organizations_controller.rb
@@ -0,0 +1,12 @@
+class OrganizationsController < ApplicationController
+  before_action :set_organization, only: :show
+
+  def show
+  end
+
+  private
+
+  def set_organization
+    @organization = Organization.find(params[:id])
+  end
+end
