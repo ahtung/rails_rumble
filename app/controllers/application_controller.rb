4fcd741e2774dd93ef6593322e5ca029a844bfb9
Onur Kucukkece <onurkucukkece@gmail.com>
2015-11-08 17:18:57 +0200

    added check screenshot action to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery
+  before_action :check_screenshot
 
   def after_sign_in_path_for(resource)
     organizations_path
+  end
+
+  private
+
+  def check_screenshot
+    if params[:screenshot].present? && params[:screenshot] == 'true'
+      render template: 'pages/screenshot'
+    end
   end
 end
