d56f14baa8785833a94ef7e032355a99def98758
Dunya Kirkali <dunyakirkali@gmail.com>
2015-11-08 14:59:51 +0100

    Clean up repo syncer

--- app/workers/organization_syncer.rb
+++ app/workers/organization_syncer.rb
@@ -6,8 +6,8 @@ class OrganizationSyncer
   def perform(id, year, user_id)
     user = User.find(user_id)
     organization = Organization.find(id)
+
     organization.fetch_repos_as_user!(user) # REQ
-    total_prog = 12 * organization.users.count * organization.repos.count
 
     for month in 1..12 do
       organization.repos.each_with_index do |repo, repo_index|
