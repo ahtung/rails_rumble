46dc3557367f97089097b23bce6f257fbf52412f
Dunya Kirkali <dunyakirkali@gmail.com>
2015-11-08 14:13:48 +0100

    Refactoring to RepoSync worker

--- app/workers/organization_syncer.rb
+++ app/workers/organization_syncer.rb
@@ -32,34 +32,7 @@ class OrganizationSyncer
       end_of_month = Date.new(year, month, 1).end_of_month
 
       organization.repos.each_with_index do |repo, repo_index|
-        repo.fetch_contributors_as_user!(user)
-        next if repo.organization.users.map(&:login).blank?
-        contributor_names = repo.organization.users.map(&:login)
-        contributor_names.each_with_index do |contributor, index|
-          begin
-            commits = user.client.commits(
-              "#{organization.name}/#{repo.name}",
-              author: contributor,
-              since: beginning_of_month,
-              until: end_of_month
-            )
-            commits.concat(user.client.last_response.rels[:next].get.data) while user.client.last_response.rels[:next]
-
-            if yearly[year][month][contributor]
-              yearly[year][month][contributor] += commits.count
-            else
-              yearly[year][month].merge!(contributor => commits.count)
-            end
-          rescue
-          end
-
-          prog += 1
-          progress = (prog.to_f / total_prog.to_f * 100.0).to_i
-          max_member = yearly[year][month].max_by{ |k,v| v }
-          best_index = contributor_names.index(max_member.first)
-          new_message = { pos: best_index, prog: progress, month: month, member: max_member, org_name: organization.name }
-          WebsocketRails[:sync].trigger('syncer', new_message)
-        end
+        RepoSyncer.perform_async(repo, user.id, year, month)
       end
       organization.update_attributes(commits: yearly)
     end
