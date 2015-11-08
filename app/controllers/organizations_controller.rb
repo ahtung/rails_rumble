2d05e4693c04c18b86e3770697bafdcbdad8f700
Dunya Kirkali <dunyakirkali@gmail.com>
2015-11-08 12:25:07 +0100

    Add organization policy

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -2,11 +2,14 @@ class OrganizationsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_year, only: [:show, :sync]
   before_action :set_organization, only: [:show, :sync]
+  after_action :verify_authorized
 
   def show
+    authorize @organization
   end
 
   def sync
+    authorize @organization
     @organization.sync!(@year, current_user)
     render layout: false
   end
