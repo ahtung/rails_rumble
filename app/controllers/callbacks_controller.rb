a34eab5a2f192ca727f8ac9f6405efae4a84e884
Dunya Kirkali <dunyakirkali@gmail.com>
2015-11-08 15:32:39 +0100

    Correct faulty fetc_orgs

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,6 +1,7 @@
 class CallbacksController < Devise::OmniauthCallbacksController
   def github
     @user = User.from_omniauth(request.env["omniauth.auth"])
+    @user.fetch_organizations!
     sign_in_and_redirect @user
   end
 
